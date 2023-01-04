b89cec0cb1182a042a0b8320a3341ba016451560
sajid-khan-afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2023-01-04 21:53:32 +0500

    Update page.tsx

--- step00_helloworld/app/name/address/page.tsx
+++ step00_helloworld/app/name/address/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   const router = useRouter();
   return (
     <>
-      <div>name (Leaf Segment) page</div>
+      <div>address (Leaf Segment) page</div>
       <br></br>
       <button onClick={() => router.push("./")}>Go back to Home page</button>
     </>
