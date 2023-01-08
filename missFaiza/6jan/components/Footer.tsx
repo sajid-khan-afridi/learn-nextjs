372314dabb72f0b9cf88f78588ae5eefe62047f4
Sajid Khan Afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2023-01-09 01:15:06 +0500

    Update Footer.tsx

--- missFaiza/6jan/components/Footer.tsx
+++ missFaiza/6jan/components/Footer.tsx
@@ -4,12 +4,30 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <>
-      {/* <nav className={styles.header}>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/about/contact">Contact Us</Link>
-      </nav> */}
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <nav className="mt-5">
+        <Link
+          className="bg-blue-500 mr-5 text-white font-bold py-2 px-4 rounded"
+          href="/"
+        >
+          Home
+        </Link>
+        <Link
+          className="bg-blue-500 mr-5 text-white font-bold py-2 px-4 rounded"
+          href="/about"
+        >
+          About
+        </Link>
+        <Link
+          className="bg-blue-500 mr-5 text-white font-bold py-2 px-4 rounded"
+          href="/about/contact"
+        >
+          Contact Us
+        </Link>
+      </nav>
+      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded">
+        hello
+      </button> */}
     </>
   );
 }
