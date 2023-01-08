839932bcbdd4f33505daa651351fab8615277696
Sajid Khan Afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2023-01-09 00:50:24 +0500

    tailwind css

---
+++ missFaiza/6jan/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <>
+      {/* <nav className={styles.header}>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/about/contact">Contact Us</Link>
+      </nav> */}
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
