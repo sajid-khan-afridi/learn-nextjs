11c5f870dfb1777a0410eb39814252cac8067fa4
sajid-khan-afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2023-01-11 17:27:30 +0500

    Update page.tsx

--- owais/9jan/app/step04/[myname]/page.tsx
+++ owais/9jan/app/step04/[myname]/page.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 
-export default function MyName({
-  params: {
-    params: { myname: string },
-  },
-}) {
+export default function MyName({ params }: { params: { myname: string } }) {
   return <div>this is url name {params.myname}</div>;
 }
