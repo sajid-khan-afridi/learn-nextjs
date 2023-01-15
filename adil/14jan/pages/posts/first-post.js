a82b2cf6a83a5572208e411b7a91154e3fa68d6a
sajid-khan-afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2023-01-15 17:17:25 +0500

    a

--- adil/14jan/pages/posts/first-post.js
+++ adil/14jan/pages/posts/first-post.js
@@ -1,14 +1,25 @@
 import Link from "next/link";
 import Head from "next/head";
+import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
     <>
-      <h1> first post </h1>
-      <Head>
-        <title>First Post</title>
-      </Head>
-      <Link href="/">Go Back</Link>
+      <Layout>
+        <h1> first post </h1>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+          }
+        />
+        <Link href="/">Go Back</Link>
+      </Layout>
     </>
   );
 }
