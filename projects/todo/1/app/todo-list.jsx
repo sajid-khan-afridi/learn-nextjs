3dadfac8a68b0fa35c7be9bd9891e451bceaf4fc
Sajid Khan Afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2023-01-18 00:52:24 +0500

    Update todo-list.jsx

--- projects/todo/1/app/todo-list.jsx
+++ projects/todo/1/app/todo-list.jsx
@@ -6,12 +6,24 @@ const getTodos = async () => {
 export default async function TodoList() {
   const { todos } = await getTodos();
   console.log(todos);
+  // const listItem=products.map(prod=><li key={prod.id} style={{
+  //   color: prod.isFruit ? 'magenta' : 'darkgreen'
+  // }}>{prod.pname}</li>)
+  // const listItem = todos.map((item) => (
+  //   <li key={item.id} style={{ padding: "5px 0" }}>
+  //     {item.name}
+  //   </li>
+  // ));
   return (
     <div>
       <ul style={{ listStyleType: "none", padding: 0 }}>
-        <li style={{ padding: "5px 0" }}>todo 1</li>
-        <li style={{ padding: "5px 0" }}>todo 2</li>
-        <li style={{ padding: "5px 0" }}>todo 3</li>
+        {todos.map((t) => (
+          <li key={t.id} style={{ padding: "5px 0" }}>
+            <input type="checkbox" />
+            {t.name}
+            <button>Delete</button>
+          </li>
+        ))}
       </ul>
     </div>
   );
