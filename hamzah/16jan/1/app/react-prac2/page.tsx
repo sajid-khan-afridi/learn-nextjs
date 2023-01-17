ded9c0fa29174cacc5649bfe0e793efc90dbcf3a
sajid-khan-afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2023-01-17 19:04:41 +0500

    a

--- hamzah/16jan/1/app/react-prac2/page.tsx
+++ hamzah/16jan/1/app/react-prac2/page.tsx
@@ -1,20 +1,37 @@
-"use client"
-import { useState } from "react"
+"use client";
+import { useState } from "react";
 
 export default function ReactComponents() {
-    const [count,setCount]=useState(0);
-    function increment(){
-        setCount(count+1);
-    }
-    function decrement(){
-        setCount(count-1);
-    }
+  const [count, setCount] = useState(0);
+  function increment() {
+    setCount(count + 1);
+  }
+  function decrement() {
+    setCount(count - 1);
+  }
   return (
-    <div>
+    <>
+      <div className="container1">
         <button onClick={increment}>++</button>
         <h1>{count}</h1>
         <button onClick={decrement}>--</button>
+      </div>
 
-    </div>
-  )
+      <div className="container2">
+        <h1>Counters that update separately</h1>
+        <MyButton />
+        <MyButton />
+      </div>
+    </>
+  );
+}
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return <button onClick={handleClick}>Clicked {count} times</button>;
 }
