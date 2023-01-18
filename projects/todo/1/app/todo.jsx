a66ddff8143a4774790f656c38c418ee9254943c
Sajid Khan Afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2023-01-18 09:50:06 +0500

    Update todo.jsx

--- projects/todo/1/app/todo.jsx
+++ projects/todo/1/app/todo.jsx
@@ -1,13 +1,18 @@
 "use client"
 
 async function update(id,isDone){
-    
+    await fetch("http://localhost:3001/api/todo/update",{
+      method:"POST",
+      mode:"no-cors",
+      body:JSON.stringify({id,isDone}),
+    }
+    )
 
 }
 export default function Todo({todo}) {
   return(
     <div>
-            <input type="checkbox" onChange={()=>}/>
+            <input type="checkbox" onChange={(e)=>{update(todo.id,e.target.checked)}}/>
             {todo.name}
             <button>Delete</button>
     </div>
