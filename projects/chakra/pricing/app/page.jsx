cfee15265b0eb05feac660759a0e1c7083d16c67
sajid-khan-afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2023-01-20 19:11:00 +0500

    a

---
+++ projects/chakra/pricing/app/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+import { Button } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <div>
+      <Button colorScheme="blue">Hello World from Panaverse DAO!</Button>
+    </div>
+  );
+}
