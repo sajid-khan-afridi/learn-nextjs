dd9f8c80a62a70e1d1b2a7d80f8c0a4796d0023f
Sajid Khan Afridi <uetianafridi@gmail.com>
2023-01-24 23:39:23 +0500

    a

---
+++ projects/todo/3/app/add-new-todo.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AddNewTodo() {
+  return <div>add-new-todo</div>;
+}
---
+++ projects/todo/3/app/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddNewTodo from "./add-new-todo";
+import TodoList from "./todo-list";
+
+export default function Page() {
+  return (
+    <div>
+      <AddNewTodo />
+      <TodoList />
+    </div>
+  );
+}
