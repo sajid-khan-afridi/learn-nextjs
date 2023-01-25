83b1b02e1e9815c2f0e2e635adf6589e8c8840b9
Sajid Khan Afridi <uetianafridi@gmail.com>
2023-01-25 06:55:00 +0500

    addnewtodo completes

--- projects/todo/3/app/add-new-todo.jsx
+++ projects/todo/3/app/add-new-todo.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+async function add(name, refresh) {
+  await fetch(`/api/todo/add`, {
+    method: "POST",
+    body: JSON.stringify({ name }),
+  });
+  refresh();
+}
 
 export default function AddNewTodo() {
-  return <div>add-new-todo</div>;
+  const [name, setName] = useState("");
+  const router = useRouter();
+  return (
+    <>
+      <input
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+      />
+      <button onClick={() => add(name, router.refresh)}> Add</button>
+    </>
+  );
 }
--- projects/todo/3/app/layout.jsx
+++ projects/todo/3/app/layout.jsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
@@ -8,7 +8,10 @@ export default function RootLayout({ children }) {
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <h1>Todo Application</h1>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
