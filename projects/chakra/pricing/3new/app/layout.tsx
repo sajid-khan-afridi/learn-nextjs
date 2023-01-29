2692bc80587226b6492d52f66a39a2b16f8a2faf
sajid-khan-afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2023-01-29 10:03:30 +0500

    a

---
+++ projects/chakra/pricing/3new/app/layout.tsx
@@ -0,0 +1,16 @@
+import ChakraWrapper from "../components/Chakra";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <head></head>
+      <body>
+        <ChakraWrapper>{children}</ChakraWrapper>
+      </body>
+    </html>
+  );
+}
--- projects/chakra/pricing/3new/app/page.tsx
+++ projects/chakra/pricing/3new/app/page.tsx
@@ -1,11 +1,10 @@
-import Header from "@/components/Header";
-
 import React from "react";
 
 export default function Page() {
   return (
     <div>
-      <Header />
+      {/* <Prac1 /> */}
+      {/* <Prac2 /> */}
     </div>
   );
 }
