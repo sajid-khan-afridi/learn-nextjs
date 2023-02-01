1dca7de41fb9402b658fef6d355460ac8a1c56f3
sajid-khan-afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2023-02-01 19:10:56 +0500

    z

--- projects/website/1/app/page.tsx
+++ projects/website/1/app/page.tsx
@@ -1,12 +1,12 @@
-"use client"
+"use client";
+import Navbar from "@/components/Navbar";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/react";
-
 
 export default function Page() {
   return (
     <ChakraProvider>
-      <Button>hi</Button>
+      <Navbar />
     </ChakraProvider>
-  )
+  );
 }
--- projects/website/1/components/Navbar.tsx
+++ projects/website/1/components/Navbar.tsx
@@ -1,12 +1,16 @@
-import { Box, Flex } from "@chakra-ui/react";
-import Link from "next/link";
+import { Box, Flex, HStack, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
 
 export default function Navbar() {
   return (
-    <Box as="nav">
-        <Link href="">
-            <Link
-        </Link>
+    <Box as="nav" bg="black" color="white">
+      <HStack>
+        <NextLink href="#" passHref>
+          <Link fontWeight={"500"} _hover={{ color: "orange" }}>
+            Docs
+          </Link>
+        </NextLink>
+      </HStack>
     </Box>
-  )
+  );
 }
