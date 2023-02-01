727c8422dc4d5e9ac4b2c3550b5f9e0492a60e50
Sajid Khan Afridi <uetianafridi@gmail.com>
2023-02-01 17:15:18 +0500

    website

---
+++ projects/website/1/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import { Box, Flex } from "@chakra-ui/react";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <Box as="nav">
+        <Link href="">
+            <Link
+        </Link>
+    </Box>
+  )
+}
