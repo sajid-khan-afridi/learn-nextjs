1a2000570fc84406626679690994f1c0eeaf5374
sajid-khan-afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2023-02-05 19:17:25 +0500

    a

---
+++ projects/website/prac/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import HCom1 from "@/components/HCom1";
+import HCom2 from "@/components/HCom2";
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      <HCom1 />
+      <HCom2 />
+    </>
+  );
+}
---
+++ projects/website/prac/components/HCom2.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { Box, Stack } from "@chakra-ui/react";
+import React from "react";
+
+export default function HCom2() {
+  return (
+    <Stack direction={"row"} alignContent="center" justifyContent={"center"}>
+      <Box> </Box>
+    </Stack>
+  );
+}
