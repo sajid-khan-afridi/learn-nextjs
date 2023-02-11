1934cc59c1ea2b1715ba875d2ab2e462a20868b0
Sajid Khan Afridi <uetianafridi@gmail.com>
2023-02-11 11:11:21 +0500

    Update Cards.tsx

--- projects/website/6/src/app/Components/Cards.tsx
+++ projects/website/6/src/app/Components/Cards.tsx
@@ -22,18 +22,19 @@ export default function Cards() {
         // spacingX="40px"
         // spacingY="20px"
       >
+        {/* section 1 */}
         <Box
           as="section"
           boxShadow={"dark-lg"}
           //   border="0.25px solid #a41e1e"
-          height="250px"
+          height={{base:"250px",md:"280px",lg:"250px"}}
           borderRadius={20}
           p="5"
         >
           <Box borderBottom={"5px solid #a41e1e"}>
             <Heading pl="5">Metaverse</Heading>
           </Box>
-          <Text fontSize={{base:"15",md:"15",lg:"15"}}>
+          <Text fontSize={{ base: "15", md: "15", lg: "15" }}>
             Panaverse DAO is a community of Web 3 and Metaverse developers,
             designers, trainers, startup founders and service providers.
             Panaverse DAO is struggling to produce professionals.
@@ -42,18 +43,47 @@ export default function Cards() {
             <Button colorScheme={"teal"}>Learn More →</Button>
           </Box>
         </Box>
+
+        {/* section 2 */}
         <Box
           boxShadow={"dark-lg"}
           //   border="0.25px solid #a41e1e"
-          height="250px"
+          height={{base:"250px",md:"280px",lg:"250px"}}
           borderRadius={20}
-        ></Box>
+          p="5"
+        >
+          <Box borderBottom={"5px solid #a41e1e"}>
+            <Heading pl="5">Blockchain</Heading>
+          </Box>
+          <Text fontSize={{ base: "15", md: "15", lg: "15" }}>
+            Panaverse DAO is a community of Web 3 and Metaverse developers,
+            designers, trainers, startup founders and service providers.
+            Panaverse DAO is struggling to produce professionals.
+          </Text>
+          <Box textAlign={"center"} pt="3">
+            <Button colorScheme={"teal"}>Learn More →</Button>
+          </Box>
+        </Box>
+        {/* section 3 */}
         <Box
           boxShadow={"dark-lg"}
           //   border="0.25px solid #a41e1e"
-          height="250px"
+          height={{base:"250px",md:"280px",lg:"250px"}}
           borderRadius={20}
-        ></Box>
+          p="5"
+        >
+          <Box borderBottom={"5px solid #a41e1e"}>
+            <Heading pl="5">Typescript</Heading>
+          </Box>
+          <Text fontSize={{ base: "15", md: "15", lg: "15" }}>
+            Panaverse DAO is a community of Web 3 and Metaverse developers,
+            designers, trainers, startup founders and service providers.
+            Panaverse DAO is struggling to produce professionals.
+          </Text>
+          <Box textAlign={"center"} pt="3">
+            <Button colorScheme={"teal"}>Learn More →</Button>
+          </Box>
+        </Box>
       </SimpleGrid>
     </Container>
   );
